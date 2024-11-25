3816cb2389303ae14ec459149fdfd30e493cc05f
Maria Leitão <malufln.99@gmail.com>
2024-11-25 05:41:31 -0300

    refactor: remove unused imports

--- src/main/java/com/vilu/pombo/controller/UsuarioController.java
+++ src/main/java/com/vilu/pombo/controller/UsuarioController.java
@@ -5,12 +5,9 @@ import com.vilu.pombo.model.entity.Usuario;
 import com.vilu.pombo.model.seletor.UsuarioSeletor;
 import com.vilu.pombo.service.UsuarioService;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
