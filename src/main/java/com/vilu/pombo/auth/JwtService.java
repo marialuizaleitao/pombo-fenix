7a34af0aa14ed686a0d990234c2d1de1a763e6f8
Maria Leitão <malufln.99@gmail.com>
2024-11-25 17:07:04 -0300

    fix: jwt service casting

--- src/main/java/com/vilu/pombo/auth/JwtService.java
+++ src/main/java/com/vilu/pombo/auth/JwtService.java
@@ -1,8 +1,11 @@
 package com.vilu.pombo.auth;
 
 import com.vilu.pombo.model.entity.Usuario;
+import com.vilu.pombo.model.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.jwt.JwtClaimsSet;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
@@ -13,37 +16,50 @@ import java.util.stream.Collectors;
 
 @Service
 public class JwtService {
+
     private final JwtEncoder jwtEncoder;
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
 
     public JwtService(JwtEncoder jwtEncoder) {
         this.jwtEncoder = jwtEncoder;
     }
 
-    public String getGenerateToken(Authentication authentication) {
+    public String getGenerateToken(Authentication subject) {
+
         Instant now = Instant.now();
 
-        // será usado para definir tempo do token
-        long dezHorasEmSegundos = 36000L;
+        long tenHoursInSeconds = 36000L;
 
-        String roles = authentication
-                .getAuthorities().stream()
+        String roles = subject
+                .getAuthorities()
+                .stream()
                 .map(GrantedAuthority::getAuthority)
-                .collect(Collectors
-                        .joining(" "));
+                .collect(Collectors.joining(" "));
 
-        Usuario usuarioAutenticado = (Usuario) authentication.getPrincipal();
+        Object principal = subject.getPrincipal();
+        Usuario authenticatedUser;
+
+        if(principal instanceof Jwt) {
+            Jwt jwt = (Jwt) principal;
+            String login = jwt.getSubject();
+
+            authenticatedUser = usuarioRepository.findByEmail(login).get();
+        } else {
+            authenticatedUser = (Usuario) principal;
+        }
 
         JwtClaimsSet claims = JwtClaimsSet.builder()
-                .issuer("pombo")                                // emissor do token
-                .issuedAt(now)                                    // data/hora em que o token foi emitido
-                .expiresAt(now.plusSeconds(dezHorasEmSegundos)) // expiração do token, em segundos.
-                .subject(authentication.getName())              // nome do usuário
-                .claim("roles", roles)                           // perfis ou permissões (roles)
-                .claim("idUsuario", usuarioAutenticado.getId()) // mais propriedades adicionais no token
+                .issuer("pombo")
+                .issuedAt(now)
+                .expiresAt(now.plusSeconds(tenHoursInSeconds))
+                .subject(subject.getName())
+                .claim("roles", roles)
+                .claim("idUsuario", authenticatedUser.getId())
                 .build();
 
         return jwtEncoder.encode(
-                        JwtEncoderParameters.from(claims))
-                .getTokenValue();
+                JwtEncoderParameters.from(claims)).getTokenValue();
     }
 }
