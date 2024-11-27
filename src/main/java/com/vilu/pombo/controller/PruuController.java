cf4b6a29774a2d1365bad5ee6b132f3d67061dae
Maria Leitão <malufln.99@gmail.com>
2024-11-27 18:55:42 -0300

    feat: fetch users who liked pruu method

--- src/main/java/com/vilu/pombo/controller/PruuController.java
+++ src/main/java/com/vilu/pombo/controller/PruuController.java
@@ -1,13 +1,5 @@
 package com.vilu.pombo.controller;
 
-import java.io.IOException;
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
 import com.vilu.pombo.auth.AuthService;
 import com.vilu.pombo.exception.PomboException;
 import com.vilu.pombo.model.entity.Pruu;
@@ -15,85 +7,100 @@ import com.vilu.pombo.model.entity.Usuario;
 import com.vilu.pombo.model.enums.Perfil;
 import com.vilu.pombo.model.seletor.PruuSeletor;
 import com.vilu.pombo.service.PruuService;
-
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/pruus")
-@CrossOrigin(origins = { "http://localhost:4200" }, maxAge = 3600)
+@CrossOrigin(origins = {"http://localhost:4200"}, maxAge = 3600)
 public class PruuController {
 
-	@Autowired
-	private PruuService pruuService;
-	@Autowired
-	private AuthService authService;
+    @Autowired
+    private PruuService pruuService;
+    @Autowired
+    private AuthService authService;
 
-	@PostMapping("/salvar-foto")
-	public void salvarImagem(@RequestParam("fotoDePerfil") MultipartFile foto, @RequestParam String pruuId) throws IOException, PomboException {
-		Usuario subject = authService.getUsuarioAutenticado();
-		if (foto == null) {
-			throw new PomboException("O arquivo inserido é inválido.", HttpStatus.BAD_REQUEST);
-		}
-		pruuService.salvarImagem(foto, pruuId, subject.getId());
-	}
-
-	@Operation(summary = "Inserir novo pruu", description = "Adiciona um novo pruu ao sistema.", responses = {@ApiResponse(responseCode = "200", description = "Pruu criado com sucesso", content = @Content(mediaType = "application/json", schema = @Schema(implementation = Pruu.class))), @ApiResponse(responseCode = "400", description = "Erro de validação ou regra de negócio", content = @Content(mediaType = "application/json", examples = @ExampleObject(value = "{\"message\": \"Erro de validação: campo X é obrigatório\", \"status\": 400}")))})
-    @PostMapping(path = "/cadastrar")
-    public ResponseEntity<Pruu> cadastrar(@Valid @RequestBody Pruu novoPruu) throws PomboException {
-    	 Usuario subject = authService.getUsuarioAutenticado();
-
-         if (subject.getPerfil() == Perfil.USUARIO) {
-             novoPruu.setUsuario(subject);
-             Pruu pruuCriado = pruuService.cadastrar(novoPruu);
-             return ResponseEntity.status(201).body(pruuCriado);
-         } else {
-             throw new PomboException("Administradores não podem criar Pruus.", HttpStatus.BAD_REQUEST);
-         }
+    @PostMapping("/salvar-foto")
+    public void salvarImagem(@RequestParam("fotoDePerfil") MultipartFile foto, @RequestParam String pruuId) throws IOException, PomboException {
+        Usuario subject = authService.getUsuarioAutenticado();
+        if (foto == null) {
+            throw new PomboException("O arquivo inserido é inválido.", HttpStatus.BAD_REQUEST);
+        }
+        pruuService.salvarImagem(foto, pruuId, subject.getId());
     }
 
-	@Operation(summary = "Curtir ou descurtir um pruu", description = "Curte ou descurte um pruu pelo seu ID.", responses = {
-			@ApiResponse(responseCode = "200", description = "Pruu curtido/descurtido com sucesso") })
-	@PostMapping(path = "/curtir/{idPruu}")
-	public ResponseEntity<Void> curtir(@PathVariable String idPruu)
-			throws PomboException {
-		pruuService.toggleCurtida(idPruu);
-		return ResponseEntity.noContent().build();
-	}
+    @Operation(summary = "Inserir novo pruu", description = "Adiciona um novo pruu ao sistema.", responses = {@ApiResponse(responseCode = "200", description = "Pruu criado com sucesso", content = @Content(mediaType = "application/json", schema = @Schema(implementation = Pruu.class))), @ApiResponse(responseCode = "400", description = "Erro de validação ou regra de negócio", content = @Content(mediaType = "application/json", examples = @ExampleObject(value = "{\"message\": \"Erro de validação: campo X é obrigatório\", \"status\": 400}")))})
+    @PostMapping(path = "/cadastrar")
+    public ResponseEntity<Pruu> cadastrar(@Valid @RequestBody Pruu novoPruu) throws PomboException {
+        Usuario subject = authService.getUsuarioAutenticado();
 
-	@Operation(summary = "Bloquear um pru", description = "Bloqueia um pruu pelo seu ID.")
-	@DeleteMapping("/excluir/{idPruu}/{idUsuario}")
-	public ResponseEntity<Void> excluir(@PathVariable String idPruu, @PathVariable String idUsuario)
-			throws PomboException {
-		pruuService.excluir(idPruu, idUsuario);
-		return ResponseEntity.noContent().build();
-	}
+        if (subject.getPerfil() == Perfil.USUARIO) {
+            novoPruu.setUsuario(subject);
+            Pruu pruuCriado = pruuService.cadastrar(novoPruu);
+            return ResponseEntity.status(201).body(pruuCriado);
+        } else {
+            throw new PomboException("Administradores não podem criar Pruus.", HttpStatus.BAD_REQUEST);
+        }
+    }
 
-	@Operation(summary = "Listar todos os pruus", description = "Retorna uma lista de todos os pruus cadastrados no sistema.", responses = {
-			@ApiResponse(responseCode = "200", description = "Lista de pruus retornada com sucesso") })
-	@PostMapping(path = "/todos")
-	public List<Pruu> pesquisarTodos(@RequestBody PruuSeletor paginacao) throws PomboException {
-		return pruuService.pesquisarTodos(paginacao);
-	}
+    @Operation(summary = "Curtir ou descurtir um pruu", description = "Curte ou descurte um pruu pelo seu ID.", responses = {
+            @ApiResponse(responseCode = "200", description = "Pruu curtido/descurtido com sucesso")})
+    @PostMapping(path = "/curtir/{idPruu}")
+    public ResponseEntity<Void> curtir(@PathVariable String idPruu)
+            throws PomboException {
+        pruuService.toggleCurtida(idPruu);
+        return ResponseEntity.noContent().build();
+    }
 
-	@Operation(summary = "Pesquisar pruu por ID", description = "Busca um pruu específico pelo seu ID.")
-	@GetMapping(path = "/{id}")
-	public ResponseEntity<Pruu> pesquisarPorId(@PathVariable String id) throws PomboException {
-		Pruu pruu = pruuService.pesquisarPorId(id);
-		return ResponseEntity.ok(pruu);
-	}
+    @Operation(summary = "Bloquear um pru", description = "Bloqueia um pruu pelo seu ID.")
+    @DeleteMapping("/excluir/{idPruu}")
+    public ResponseEntity<Void> excluir(@PathVariable String idPruu)
+            throws PomboException {
+        Usuario subject = authService.getUsuarioAutenticado();
+        pruuService.excluir(idPruu, subject.getId());
+        return ResponseEntity.noContent().build();
+    }
 
-	@Operation(summary = "Pesquisar com filtro", description = "Retorna uma lista de pruus de acordo com o filtro selecionado.", responses = {
-			@ApiResponse(responseCode = "200", description = "Pruus filtrados com sucesso", content = @Content(mediaType = "application/json", schema = @Schema(implementation = Pruu.class))),
-			@ApiResponse(responseCode = "500", description = "Erro interno do servidor", content = @Content(mediaType = "application/json", schema = @Schema(description = "Detalhes do erro interno", example = "{\"message\": \"Erro interno do servidor\", \"status\": 500}"))) })
-	@PostMapping("/filtrar")
-	public List<Pruu> pesquisarComFiltros(@RequestBody PruuSeletor seletor) throws PomboException {
-		return pruuService.pesquisarComFiltros(seletor);
-	}
+    @Operation(summary = "Listar todos os pruus", description = "Retorna uma lista de todos os pruus cadastrados no sistema.", responses = {
+            @ApiResponse(responseCode = "200", description = "Lista de pruus retornada com sucesso")})
+    @PostMapping(path = "/todos")
+    public List<Pruu> pesquisarTodos(@RequestBody PruuSeletor paginacao) throws PomboException {
+        return pruuService.pesquisarTodos(paginacao);
+    }
+
+    @Operation(summary = "Pesquisar pruu por ID", description = "Busca um pruu específico pelo seu ID.")
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<Pruu> pesquisarPorId(@PathVariable String id) throws PomboException {
+        Pruu pruu = pruuService.pesquisarPorId(id);
+        return ResponseEntity.ok(pruu);
+    }
+
+    @Operation(summary = "Pesquisar com filtro", description = "Retorna uma lista de pruus de acordo com o filtro selecionado.", responses = {
+            @ApiResponse(responseCode = "200", description = "Pruus filtrados com sucesso", content = @Content(mediaType = "application/json", schema = @Schema(implementation = Pruu.class))),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor", content = @Content(mediaType = "application/json", schema = @Schema(description = "Detalhes do erro interno", example = "{\"message\": \"Erro interno do servidor\", \"status\": 500}")))})
+    @PostMapping("/filtrar")
+    public List<Pruu> pesquisarComFiltros(@RequestBody PruuSeletor seletor) throws PomboException {
+        return pruuService.pesquisarComFiltros(seletor);
+    }
+
+    @Operation(summary = "Pesquisar usuários que curtiram", description = "Retorna uma lista de usuários que curtiram determinada postagem.", responses = {
+            @ApiResponse(responseCode = "200", description = "Usuários listados com sucesso", content = @Content(mediaType = "application/json", schema = @Schema(implementation = Pruu.class))),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor", content = @Content(mediaType = "application/json", schema = @Schema(description = "Detalhes do erro interno", example = "{\"message\": \"Erro interno do servidor\", \"status\": 500}")))})
+    @PostMapping("/usuarios-que-curtiram")
+    public List<Usuario> pesquisarUsuariosQueCurtiram(@PathVariable String pruuId) throws PomboException {
+        return pruuService.pesquisarUsuariosQueCurtiram(pruuId);
+    }
 
 }
